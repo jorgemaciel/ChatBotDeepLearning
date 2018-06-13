91bd03c9d392a0225b4f213f09d376fdfa2207f0
jmaciel <jorge.maciel.jr@gmail.com>
2018-06-13 09:30:49 -0300

    Setting the Hyperparameters and session

--- chatbot.py
+++ chatbot.py
@@ -297,3 +297,60 @@ def seq2seq_model(inputs, targets, keep_prob, batch_size, sequence_length,
 
     return training_predictions, test_predictions
 
+# TRAINING THE SEQ2SEQ MODEL
+
+# Setting the Hyperparameters
+epochs = 100
+batch_size = 64
+rnn_size = 512
+num_layers = 3
+encoding_embedding_size = 512
+decoding_embedding_size = 512
+learning_rate = 0.01
+learning_rate_decay = 0.9
+min_learning_rate = 0.0001
+keep_probability = 0.5
+
+# Defining a session()
+tf.reset_default_graph()
+session = tf.InteractiveSession()
+
+# Loading the model inputs
+inputs, targets, lr, keep_prob = model_inputs()
+
+# Setting the sequence length
+sequence_length = tf.placeholder_with_default(25, None, name='sequence_length')
+
+# Getting the shape of the inputs tensor
+input_shape = tf.shape(inputs)
+
+# Getting the training and test predictions
+training_predictions, test_predictions = seq2seq_model(tf.reverse(inputs, [-1]),
+                                                       targets,
+                                                       keep_prob,
+                                                       batch_size,
+                                                       sequence_length,
+                                                       len(answersword2int),
+                                                       len(questionwords2int),
+                                                       encoding_embedding_size,
+                                                       decoding_embedding_size,
+                                                       rnn_size,
+                                                       num_layers,
+                                                       questionwords2int)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
